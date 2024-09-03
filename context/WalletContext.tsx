75597981ace74df5b2933746530ceb2b67d87592
fabiofabrice <fabricesumsa2000@gmail.com>
2024-09-03 10:59:52 +0200

    add MWA

--- context/WalletContext.tsx
+++ context/WalletContext.tsx
@@ -1,14 +1,14 @@
 "use client";
-
 import React, { FC, ReactNode, useMemo } from "react";
 import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
-import { SolanaMobileWalletAdapter,
-    createDefaultAuthorizationResultCache,
-    createDefaultAddressSelector,
- } from "@solana-mobile/wallet-adapter-mobile";
+import {
+  SolanaMobileWalletAdapter,
+  createDefaultAuthorizationResultCache,
+  createDefaultAddressSelector,
+} from "@solana-mobile/wallet-adapter-mobile";
 import { clusterApiUrl } from "@solana/web3.js";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 
@@ -16,12 +16,12 @@ interface WalletContextProviderProps {
     children: ReactNode;
 }
 
-const WalletContextProvider: FC<WalletContextProviderProps> = ({ children })  => {
+const WalletContextProvider: FC<WalletContextProviderProps> = ({ children }) => {
     const network = WalletAdapterNetwork.Mainnet;
-    const endpoint = useMemo(() => clusterApiUrl(network), []); // Use "mainnet-beta" for mainnet
+    const endpoint = useMemo(() => clusterApiUrl(network), []);
 
     const mobileWallet = useMemo(() => {
-        return new SolanaMobileWalletAdapter({
+        const adapter = new SolanaMobileWalletAdapter({
             cluster: network,
             appIdentity: {
                 name: "Breakfast",
@@ -31,18 +31,26 @@ const WalletContextProvider: FC<WalletContextProviderProps> = ({ children })  =>
             authorizationResultCache: createDefaultAuthorizationResultCache(),
             addressSelector: createDefaultAddressSelector(),
             onWalletNotFound: async (mobileWalletAdapter) => {
-                console.error('Wallet not found!',);
+                console.error('Wallet not found!');
             },
         });
+
+        // Manually add required properties
+        adapter.name = "Solana Mobile Wallet";
+        adapter.url = "https://solana.com";
+        adapter.icon = "https://rune-on-solana.onrender.com/asset/breakfast_mini.png";
+        adapter.ready = true;
+
+        return adapter;
     }, [network]);
 
-  return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={[mobileWallet]}>
-        {children}
-      </WalletProvider>
-    </ConnectionProvider>
-  );
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={[mobileWallet]}>
+                {children}
+            </WalletProvider>
+        </ConnectionProvider>
+    );
 };
 
 export default WalletContextProvider;
